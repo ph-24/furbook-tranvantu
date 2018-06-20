a3dc94eca4570fe25929a6971bb089c30a59b702
Tran Van Tu <tranvantu.dev@gmail.com>
2018-06-20 19:13:30 +0700

    Create view of cats

---
+++ resources/views/cats/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('header')
+	@if(isset($breed))
+		<a href="{{ url('/') }}">Back to the overview</a>
+	@endif
+	<h2>
+		All @if(isset($breeds)){{ $breed->name }} @endif Cats
+
+		<a href="{{ url('cats/create') }}" class="btn btn-primary">Add a new cat</a>
+	</h2>
+	@stop
+	@section('content')
+		@foreach($cats as $cat)
+		<div class="cat">
+			<a href="{{ url('cats/'.$cat->id) }}">
+				<strong>{{ $cat->name }}</strong> - {{$cat->breed->name}}
+			</a>
+		</div>
+		@endforeach
+@stop
